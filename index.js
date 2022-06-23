da1ac53cb0ee98198489f8e44a8f913a45f93c47
playmaker159 <ashwinprabhu2001@gmail.com>
2022-06-23 07:01:46 +0530

    progress

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 const url = `mongodb+srv://ashwin:1234@cluster0.cjcyr.mongodb.net/socialDB?retryWrites=true&w=majority`;
 
@@ -40,7 +42,15 @@ app.get('/', (req, res) => {
 }
 );
 
-app.post('/register', (req, res) => {
+app.get('/home', (req, res) => {
+    res.render('home')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
+app.post('/auth/register', (req, res) => {
     const { name, email, password } = req.body;
     bcrypt.hash(password, saltRounds, function (err, hash) {
         const user = new User({
@@ -61,18 +71,34 @@ app.post('/register', (req, res) => {
 }
 );
 
-app.post('/login', (req, res) => {
+
+const loggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+app.get('/login', (req, res) => {
+    res.render('login')
+}
+);
+
+app.post('/auth/login',async(req, res) => {
     const { email, password } = req.body;
-    User.findOne({ email }).then((user) => {
+    await User.findOne({ email }).then((user) => {
         if (!user) {
             res.send('User not found');
         } else {
-            bcrypt.compare(password, user.password, function (err, result) {
+            bcrypt.compare(password, user.password,async function (err, result) {
                 if (result) {
-                    res.send('User found');
+                    // res.send('User found');
+                    res.redirect('/home');
+                    req.user =await user;
+                    console.log(user);
                     // set a header if user is logged in
-                    res.setHeader('x-auth', user.token);
-
+                    // res.setHeader('x-auth', user.token);
                 } else {
                     res.send('Incorrect password');
                 }
